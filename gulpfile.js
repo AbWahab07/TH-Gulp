3d6b992d13308927e07d8179037c141fc77b38b9
Abdul Wahab <abwahab708@gmail.com>
2018-11-07 21:24:52 +0500

    Minified the js file and renamed the file on the fly using gulp-rename package

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    jsbeautifier = require('gulp-jsbeautifier');
+    ;
 
 gulp.task('concatScripts', () => {
   gulp.src([
@@ -9,6 +13,22 @@ gulp.task('concatScripts', () => {
       .pipe(concat("app.js"))
       .pipe(gulp.dest("js"));
 });
+
+gulp.task('minifyScripts', () => {
+  gulp.src('js/app.js')
+      .pipe(uglify())
+      .pipe(rename('app.min.js'))
+      .pipe(gulp.dest('js'));
+});
+
+/*
+gulp.task('prettify', function() {
+  gulp.src('js/app.min.js')
+    .pipe(jsbeautifier())
+    .pipe(rename('app.beautify.js'))
+    .pipe(gulp.dest('js'));
+});
+*/
 
 gulp.task('default', ['concatScripts'], () => {
   console.log('Default Task');
